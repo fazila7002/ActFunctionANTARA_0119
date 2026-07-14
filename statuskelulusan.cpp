72367414ddaba9eb94f49a699f8146ff9fec92c1
fazila7002 <fazilatunnisa705@gmail.com>
2026-07-14 10:29:41 +0700

    fungsi utama, disini bakalan di kembalikan ke variabel aslinya dan di akhir ada output untuk memasukkan nilai serta akan di panggil fungsi untuk menampilkan status kelulusannya

--- statuskelulusan.cpp
+++ statuskelulusan.cpp
@@ -14,3 +14,15 @@ string CekStatusBerparameter(float a) {
     }
 
 }
+
+int main () 
+{
+    float Ujian1, Ujian2;
+
+    cout << "Masukkan Nilai Ujian 1 : ";
+    cin >> Ujian1;
+    cout << "Masukkan Nilai Ujian 2 : ";
+    cin >> Ujian2;
+    cout << "Status Kelulusan : "
+    << CekStatusBerparameter(HitungRerataBerparameter(Ujian1, Ujian2));
+}
